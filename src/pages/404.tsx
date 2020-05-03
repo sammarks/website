fb46460a6607dada6320cbdb3f69505b08c043e4
Sam Marks <sam@sammarks.me>
2020-05-03 19:12:34 -0400

    feat: add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-
+import { BodyLarge, H1, Body, Block } from '@sammarks/web'
+import styled from 'styled-components'
 import IndexLayout from '../layouts'
+import { PageTitle } from '../components/PageTitle'
+import { Container } from '../components/styled'
+import { Header } from '../components/Header'
+
+const ContentContainer = styled.div`
+  text-align: center;
+`
 
 const NotFoundPage = () => (
   <IndexLayout>
-    <h1>404: Page not found.</h1>
-    <p>
-      You've hit the void. <Link to={'/'}>Go back.</Link>
-    </p>
+    <PageTitle title={'404'} />
+    <Container>
+      <Header />
+      <ContentContainer>
+        <Block marginTop={'@size-xxl'}>
+          <H1>Page not found.</H1>
+          <BodyLarge isBlock>Whoops! The page you're trying to find isn't here.</BodyLarge>
+          <Body>
+            <Link to={'/'}>&larr; Go home</Link>
+          </Body>
+        </Block>
+      </ContentContainer>
+    </Container>
   </IndexLayout>
 )
 
