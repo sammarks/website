75a6ce13823d8b96bab8594c54276465ecb574b3
Sam Marks <sam@sammarks.me>
2020-05-03 16:57:19 -0400

    fix: ignore irrelevant lighthouse audits

--- lighthouse-config.js
+++ lighthouse-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   settings: {
     skipAudits: [
-      'uses-http2' // This will never be the case when testing in CI.
+      'uses-http2', // This will never be the case when testing in CI.
+      'canonical', // This is because we're testing using a local server in CI, so the improper domain.
+      'redirects-http' // Same as above - not the real production environment.
     ]
   }
 }
